d90a0796e5821aa2809bd49204106777fa9f6292
Juani Sacco <soii.juaany@hotmail.com>
2019-06-22 03:53:03 -0300

    some fixes

--- assets/js/calcViewPort.js
+++ assets/js/calcViewPort.js
@@ -2,6 +2,11 @@ var vh = window.innerHeight * 0.01,
     vw = window.innerWidth * 0.01;
 		
 window.addEventListener('resize', () => {
+	resizeWin;
+});
+
+
+function resizeWin(){
 	document.documentElement.style.setProperty('--vh', vh+"px");
 	document.documentElement.style.setProperty('--vw', vw+"px");
-});
+}
