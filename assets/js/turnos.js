f8e3f3298192506e108d1fcce70460179617f25d
Juani Sacco <soii.juaany@hotmail.com>
2019-07-04 19:43:23 -0300

    Registrar turnos by steps

--- assets/js/turnos.js
+++ assets/js/turnos.js
@@ -3,12 +3,23 @@ var document = document || {},
     window = window || {},
     turnos = turnos || {},
     btnEditar,
-    divForm;
+    divForm,
+    currentTab = 0;
 
 turnos.init = function(){
   window.addEventListener("DOMContentLoaded", function() {
-      btnEditar = document.getElementById('btnEditar');
-      divForm = document.getElementById('nuevoTurno');
+    btnEditar = document.getElementById('btnEditar');
+    divForm = document.getElementById('nuevoTurno');
+	
+	var tipoTurno = divForm.getElementsByTagName('form')[0].tipo_turno;
+	tipoTurno.addEventListener('change',function(){
+		if(tipoTurno.value == 1){
+			//tipoTurno.value=1;
+			document.querySelector("#eq_rival").style.display = "inline-block";
+		}else{
+			document.querySelector("#eq_rival").style.display = "none";
+		}
+	});
   });
 }
 
@@ -52,12 +63,13 @@ turnos.getCanchas = function(){
 
 turnos.toggle = function(){
   var style = divForm.style.display;
-  if(style == 'flex'){
-    divForm.style.display = 'none';
-  }else {
-    divForm.style.display='flex';
-    turnos.getCanchas();
-  }
+	if(style == 'flex'){
+		divForm.style.display = 'none';
+	}else{
+		divForm.style.display='flex';
+		turnos.getCanchas();
+		turnos.showTab(currentTab);
+	}
 }
 
 turnos.crearTurno = function(){
@@ -88,4 +100,85 @@ turnos.crearTurno = function(){
       }else{
         alert("Los Datos ingresados son Incorrectos!");
       }
+}
+
+
+
+turnos.showTab = function(n) {
+	var x = document.getElementsByClassName("stepTab");
+	x[n].style.display = "flex";
+	if (n == 0) {
+		document.getElementById("prevBtn").style.display = "none";
+	} else {
+		document.getElementById("prevBtn").style.display = "inline";
+	}
+	if (n == (x.length - 1)) {
+		//Termino! 
+		var divResumen = document.querySelector("#resumenTurno"),
+			form = document.turno,
+			tipo_turno = form.tipo_turno.selectedOptions[0],
+			fecha_turno = form.fecha_turno.value,
+			horario_turno = form.horario_turno.value,
+			cancha_turno = form.cancha_turno.selectedOptions[0].innerHTML;
+			
+		document.getElementById("nextBtn").style.display = "none";
+		divResumen.children[0].innerHTML += ' '+tipo_turno.innerHTML;
+		if (tipo_turno == 1){
+			divResumen.children[0].innerHTML += ' Equipo rival: '+form.rival.value;
+		}
+		divResumen.children[1].innerHTML += ' '+cancha_turno;
+		divResumen.children[2].innerHTML += ' '+fecha_turno;
+		divResumen.children[3].innerHTML += ' '+horario_turno;
+	} else {
+		document.getElementById("nextBtn").style.display = "inline";
+		document.getElementById("nextBtn").innerHTML = "Siguiente";
+	}
+	turnos.updStepIndicator(n)
+}
+
+turnos.updStepIndicator = function(n) {
+	// This function removes the "active" class of all steps...
+	var i, x = document.getElementsByClassName("step");
+	for (i = 0; i < x.length; i++) {
+	x[i].classList.remove("active");
+	}
+	//... and adds the "active" class to the current step:
+	x[n].classList.add("active");
+}
+
+
+turnos.nextPrevTab = function(n) {
+  var x = document.getElementsByClassName("stepTab");
+  if (n == 1 && !turnos.validateForm()) return false;
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  if (currentTab >= x.length) {
+    document.querySelector("#nuevoTurno form").submit();
+    return false;
+  }
+  turnos.showTab(currentTab);
+}
+
+//Solo verifica que esten llenos los input. TODO: Agregar alguna verificacion extra
+turnos.validateForm = function () {
+  var x, y, z, i, valid = true;
+  x = document.getElementsByClassName("stepTab");
+  y = x[currentTab].getElementsByTagName("input");
+  z = x[currentTab].getElementsByTagName("select");
+  for (i = 0; i < y.length; i++) {
+    if (y[i].value == "") {
+      y[i].className += " invalid";
+      valid = false;
     }
+  }
+  for (i = 0; i < z.length; i++) {
+    if (z[i].value == "") {
+      z[i].className += " invalid";
+      valid = false;
+    }
+  }
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid;
+}
